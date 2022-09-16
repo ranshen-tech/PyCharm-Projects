ae9862cbf46b0cea36316a0e1567ae6a459515ba
Ran Shen <ranshen0519@icloud.com>
2022-09-16 14:26:12 +0800

    Changed

---
+++ Memo/Python/基础题100例.py
@@ -0,0 +1,32 @@
+# 2.数字阶乘
+"""
+6的阶乘：6*5*4*3*2*1
+3的阶乘：3*2*1
+"""
+
+
+# def factorial(number):
+#     result = 1
+#     origin = number
+#     while number > 0:
+#         result *= number
+#         number -= 1
+#     print(f'{origin}的阶乘是{result}')
+#
+#
+# factorial(3)
+# factorial(6)
+# factorial(10)
+
+
+# 3.求圆的面积
+# import math
+#
+#
+# def area_of_circle(r):
+#     return f'半径为{r}的圆的面积是：{round(math.pi * r ** 2, 2)}'
+#
+#
+# print(area_of_circle(2))
+# print(area_of_circle(3.14))
+# print(area_of_circle(6.78))
