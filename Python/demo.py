737b8f7075b88bdbb61ae79094dd790257f300af
Ran Shen <ranshen0519@icloud.com>
2022-09-20 20:49:38 +0800

    Changed

--- Python/demo.py
+++ Python/demo.py
@@ -2,57 +2,6 @@
 # print(a, b, d)
 
 
-# tuple1 = (i for i in range(10) if i % 2 == 0)
-# print(tuple1)
-# print(tuple(tuple1))
-
-
-# top = int(input('请输入菱形上半部分有多少行：'))
-# bot = top - 1
-#
-# for i in range(top):
-#     for j in range(bot - i):
-#         print(' ', end='')
-#     print('*' * (1 + 2 * i))
-#
-# for i in range(bot):
-#     for j in range(i + 1):
-#         print(' ', end='')
-#     print('*' * (2 * (bot - i) - 1))
-
-
-# for i in 'python':
-#     for j in range(2):
-#         print(i, end='')
-#         if i == 'h':
-#             break
-
-
-# import random
-
-
-# lst = [random.random() for _ in range(10)]
-# print(lst)
-
-
-# lst = [[j for j in range(3)] for i in range(4)]
-# print(lst)
-
-#
-# # zip函数的使用  函数映射的结果是zip对象
-# lst1 = [1, 2, 3, 4]
-# lst2 = ['rs', 'zyc', 'cyy', 'xp']
-# zip1 = zip(lst1, lst2)
-# zip2 = zip(lst2, lst1)
-# print(zip1)
-# print(zip2)
-# print(list(zip1))
-# print(list(zip2))
-# d = dict(zip1)
-# print(d)
-# print(dict(zip2))
-
-
 # 使用参数创建字典
 # d = dict(cat=10, dog=20)  # 参数相当于变量，变量的名字不加引号
 # print(d)
@@ -64,68 +13,37 @@
 # print(d)
 
 
-# # pop方法
-# print(d.pop(1))
-# print(d)
-# print(d.pop(111, '不存在'))
-# print(d)
-
-
 # 字典生成
 # import random
-
-
 # d = {key: random.randint(1, 3) for key in range(5)}
 # print(d)
 
 
-# # 使用映射函数生成字典
+# 使用映射函数生成字典
 # name = ['rs', 'zyc', 'cyy']
 # age = [29, 18, 21]
 # d = {key: value for key, value in zip(name, age)}
 # print(d)
 
 
-# 练习
-# d = {'a': 10, 'b': 20, 'c': 30, 'd': 40}
-# d2 = d
-# d['b'] = 100
-# print(d['b'] + d2['b'])
-
-
-# lst = []
-# for i in '想念':
-#     for j in '家人':
-#         lst.append(i + j)
-# print(lst)
-
-
-# 2022918
 # lst1 = [1, 2, 3, 4, 5]
 # lst2 = ['python', 'java', 'javascript', 'rust']
 # z = zip(lst1, lst2)
+# print(z)
 # x = tuple(z)
+# print(x)
 # print(dict(tuple(zip(lst1, lst2))))
+
 
 # a = (('apple', 'a'), ('b', 'banana'), ('c', 'cat'))
 # b = list(a)
 # print(b)
 # print(dict(b))
+# print(dict(a))
 
-
-# t = (i for i in range(10) if i % 2)
-# print(tuple(t))
 
 # d = {i: i + 2 for i in range(10) if i % 2}
 # print(d)
 
 
 # 集合是可变数据类型，集合中的元素必须是不可变数据类型,集合没有索引，但集合中元素可以for遍历
-# lst = (1, 2, 3)
-# s = set(lst)
-# print(s)
-# s.add(22)
-# print(s)
-# for i in s:
-#     print(i)
-
