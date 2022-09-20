262f0c85699e61c9d09a222cd7006426f3f56dc7
Ran Shen <ranshen0519@icloud.com>
2022-09-20 10:08:04 +0800

    Updated

--- Python/aaaaa.py
+++
@@ -1,7 +0,0 @@
-import json
-# 准备列表，列表中每一个元素都是字典，将其转换为json
-data = [{'name': 'rs', 'age': 19}, {'name': 'cyy', 'age': 19}]
-# data = [{'name':'rs', age = 29}, {'name':'zyc', age = 38}, {'name':'cyy', age = 16}]
-# 通过json.dump(data)方法把python数据转化为json数据
-# data = json.dump(data)
-# 通过json.load(data)方法
