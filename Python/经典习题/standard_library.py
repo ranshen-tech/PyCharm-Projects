29b0427203b4c202023d159b40655e2e779e6a0b
Ran Shen <ranshen0519@icloud.com>
2022-09-22 09:21:05 +0800

    Changed

--- Python/经典习题/standard_library.py
+++ Python/经典习题/standard_library.py
@@ -1,44 +1,78 @@
-import time
-import turtle as t
-t.penup()
-t.goto(-200, -50)
-t.pendown()
-t.begin_fill()
-t.color('red')
-t.circle(40, steps=3)  # 三角形
-t.end_fill()
+"""
+turtle库使用
+"""
+# import time
+# import turtle as t
+# t.penup()
+# t.goto(-200, -50)
+# t.pendown()
+# t.begin_fill()
+# t.color('red')
+# t.circle(40, steps=3)  # 三角形
+# t.end_fill()
 
-t.penup()
-t.goto(-100, -50)
-t.pendown()
-t.begin_fill()
-t.color('blue')
-t.circle(40, steps=4)  # 四边形
-t.end_fill()
+# t.penup()
+# t.goto(-100, -50)
+# t.pendown()
+# t.begin_fill()
+# t.color('blue')
+# t.circle(40, steps=4)  # 四边形
+# t.end_fill()
 
-t.penup()
-t.goto(0, -50)
-t.pendown()
-t.begin_fill()
-t.color('green')
-t.circle(40, steps=5)  # 五边形
-t.end_fill()
+# t.penup()
+# t.goto(0, -50)
+# t.pendown()
+# t.begin_fill()
+# t.color('green')
+# t.circle(40, steps=5)  # 五边形
+# t.end_fill()
 
-t.penup()
-t.goto(100, -50)
-t.pendown()
-t.begin_fill()
-t.color('yellow')
-t.circle(40, steps=6)
-t.end_fill()
+# t.penup()
+# t.goto(100, -50)
+# t.pendown()
+# t.begin_fill()
+# t.color('yellow')
+# t.circle(40, steps=6)
+# t.end_fill()
 
-t.penup()
-t.goto(200, -50)
-t.pendown()
-t.begin_fill()
-t.color('purple')
-t.circle(40)  # 圆
-t.end_fill()
-t.hideturtle()  # 把笔隐藏
+# t.penup()
+# t.goto(200, -50)
+# t.pendown()
+# t.begin_fill()
+# t.color('purple')
+# t.circle(40)  # 圆
+# t.end_fill()
+# t.hideturtle()  # 把笔隐藏
 
-time.sleep(1)
+# time.sleep(1)
+
+
+"""
+random库使用
+"""
+import random
+# random.seed(10)  # 种子默认是系统时间
+# print(random.random())
+# print(random.random())
+# random.seed(10)  # 种子相同，产生的随机数也相同
+# print(random.random())
+# print(random.random())
+
+# ran = random.randrange(1, 4, 2)  # [start, stop, step)
+# print(ran)
+
+# ran = random.uniform(1, 2)  # 生成一个随机数
+# print(ran)
+
+# print(random.choice('python'))
+
+# lst = [1, 2, 3]
+# random.shuffle(lst)  # 打乱列表lst位置，并返回None
+# print(lst)
+# print(random.sample(lst, 2))  # 选择样本，抽奖
+
+
+"""
+time库使用
+"""
+print()
