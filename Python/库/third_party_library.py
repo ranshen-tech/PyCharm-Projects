8b52d9cd969d9ed7a681b68dcea5600834834624
Ran Shen <ranshen0519@icloud.com>
2022-09-22 11:33:03 +0800

    Changed

--- Python/库/third_party_library.py
+++ Python/库/third_party_library.py
@@ -12,17 +12,20 @@ jieba库：将一段中文文本分割成中文词语的序列
 wordcloud库：根据文本产生词云,默认空格或标点分隔符对目标文本进行分词处理。
 对于中文文本，一般先将文本分词处理，然后以空格拼接，然后调用wordcloud库函数
 """
-# from wordcloud import WordCloud  # 从库中导入其中一个类
-# txt = 'I like Python, I am learning Python.'
+from wordcloud import WordCloud  # 从库中导入其中一个类
+txt = 'I like Python, I am learning Python.'
 # f = WordCloud().generate(txt)
+wordcloud = WordCloud().generate(txt)
+wordcloud.to_file('abc.jpg')
 # f.to_file('wordcloud.jpg')  # 这个挺关键
 
 
-import jieba
-from wordcloud import WordCloud
-txt = '程序设计语言是计算机能够理解和识别用户操作意图的一种交互体系，它按照特定规则组织、执行计算机指令，使计算机能够自动进行各种运算处理'
-words = jieba.lcut(txt)
-new_txt = ' '.join(words)
-print(new_txt)
-wordcloud = WordCloud(font_path='System/Library/fonts/PingFang.ttc').generate(new_txt)
-wordcloud.to_file('wordcloud2.jpg')
+# import jieba
+# from wordcloud import WordCloud
+# txt = '程序设计语言是计算机能够理解和识别用户操作意图的一种交互体系，它按照特定规则组织、执行计算机指令，使计算机能够自动进行各种运算处理'
+# words = jieba.lcut(txt)
+# new_txt = ' '.join(words)
+# print(new_txt)
+# wordcloud = WordCloud(font_path='System/Library/fonts/PingFang.ttc').generate(new_txt)
+# wordcloud.to_file('wordcloud2.jpg')
+
