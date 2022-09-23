9d49f6da92388fc1adbc756117210257ddb7e238
Ran Shen <ranshen0519@icloud.com>
2022-09-23 21:25:15 +0800

    Changed

--- 计算机二级模拟题/全国计算机等级二级考试模拟试卷二/源码/综合应用题1.py
+++ 计算机二级模拟题/全国计算机等级二级考试模拟试卷二/源码/综合应用题1.py
@@ -1,26 +1,17 @@
+"""
+问题1：请编写程序，对《天龙八部》文本中出现的汉字和标点符号进行统计
+字符与出现次数之间用冒号分隔，输出保存以“天龙八部-汉字统计.txt”文件中
+该文件要求使用CSV格式存储，参数格式如下（注意，不统计空格和回车字符）
+"""
 
+with open('天龙八部-网络版.txt') as f:
+    with open('天龙八部-汉字统计.csv', 'w') as fw:
+        txt = f.read().strip()
+        d = {}
+        for i in txt.replace(' ', '').replace('\n', ''):
+            d[i] = d.get(i, 0) + 1
+        lst = []
+        for key in d:
+            lst.append(f"{key}: {d[key]}")
+        fw.write(','.join(lst))
 
-# with open('天龙八部-网络版.txt') as f:
-#     with open('天龙八部-汉字统计.txt', 'w') as fw:
-#         txt = f.read()
-#         d = {}
-#         for i in txt:
-#             d[i] = d.get(i, 0) + 1
-#         lst = []
-#         for key in d:
-#             lst.append(f"{key}: {d[key]}")
-#         fw.write(','.join(lst))
-
-
-with open('ran.txt') as f:
-    # with open('ran1.txt', 'w') as fw:
-    txt = f.read()
-    a = txt.strip()
-    print(a)
-        # d = {}
-        # for i in txt:
-        #     d[i] = d.get(i, 0) + 1
-        # lst = []
-        # for key in d:
-        #     lst.append(f'{key}: {d[key]}')
-        # fw.write(','.join(lst))
--- 计算机二级模拟题/全国计算机等级二级考试模拟试卷二/源码/综合应用题2.py
+++ 计算机二级模拟题/全国计算机等级二级考试模拟试卷二/源码/综合应用题2.py
@@ -1,17 +1,20 @@
 import jieba
-fi=open('天龙八部-网络版.txt','r',encoding='utf-8')
-fo=open('天龙八部-词语统计.txt','w',encoding='utf-8')
-txt=fi.read()
-txt=jieba.lcut(txt)
-#print(len(txt))
-d={}
-for word in txt:
-    d[word]=d.get(word,0)+1
 
-lst=[]
+fi = open('天龙八部-网络版.txt', 'r', encoding='utf-8')
+fo = open('天龙八部-词语统计.txt', 'w', encoding='utf-8')
+txt = fi.read()
+txt = jieba.lcut(txt)
+# print(len(txt))
+d = {}
+for word in txt:
+    d[word] = d.get(word, 0) + 1
+
+lst = []
 for key in d:
-    lst.append('{}:{}'.format(key,d[key]))
+    lst.append('{}:{}'.format(key, d[key]))
 
 fo.write(','.join(lst))
 fi.close()
 fo.close()
+
+
