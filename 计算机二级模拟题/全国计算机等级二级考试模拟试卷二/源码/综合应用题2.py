0bc2c8f79907f3033e451e15aee65ca484565ba2
Ran Shen <ranshen0519@icloud.com>
2022-09-23 21:53:30 +0800

    Changed

--- 计算机二级模拟题/全国计算机等级二级考试模拟试卷二/源码/综合应用题2.py
+++ 计算机二级模拟题/全国计算机等级二级考试模拟试卷二/源码/综合应用题2.py
@@ -1,20 +1,13 @@
 import jieba
 
-fi = open('天龙八部-网络版.txt', 'r', encoding='utf-8')
-fo = open('天龙八部-词语统计.txt', 'w', encoding='utf-8')
-txt = fi.read()
-txt = jieba.lcut(txt)
-# print(len(txt))
-d = {}
-for word in txt:
-    d[word] = d.get(word, 0) + 1
-
-lst = []
-for key in d:
-    lst.append('{}:{}'.format(key, d[key]))
-
-fo.write(','.join(lst))
-fi.close()
-fo.close()
-
+with open('天龙八部-网络版.txt') as f:
+    with open('天龙八部-词语统计.txt', 'w') as fw:
+        txt = jieba.lcut(f.read().replace(' ', '').replace('\n', '').replace('。', ''))
+        d = {}
+        for word in txt:
+            d[word] = d.get(word, 0) + 1
+        lst = []
+        for key in d:
+            lst.append(f'{key}: {d[key]}')
+        fw.write(','.join(lst))
 
