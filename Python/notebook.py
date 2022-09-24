bd7abb1d9d8965844a1a5a93fea83506016c2c96
Ran Shen <ranshen0519@icloud.com>
2022-09-24 14:11:30 +0800

    Changed

--- Python/notebook.py
+++ Python/notebook.py
@@ -305,10 +305,9 @@
 # class Clock:
 #     id = None  # 序列
 #     price = None  # 价格
-
-    # def ring(self):
-    #     import os
-    #     os.system("say 'hi'")
+# def ring(self):
+#     import os
+#     os.system("say 'hi'")
 
 # 构建对象
 # clock1 = Clock()
@@ -316,5 +315,35 @@
 # clock1.price = 19.99
 # print(f'⏰id：{clock1.id}, 价格：{clock1.price}')
 # clock1.ring()
+
+
+# class Student:
+#     name = None
+#     age = None
+#     tel = None
+# def __init__(self, name, age, tel):
+#     self.name = name
+#     self.age = age
+#     self.tel = tel
+#     print('Student类创建了一个类对象')
+# stu = Student('周杰伦', 31, '1850000666')
+# print(stu.name)
+# print(stu.age)
+# print(stu.tel)
+
+
+# 练习
+# class Student:
+#     def __init__(self, name, age, addr):  # 构造方法
+#         self.name = name
+#         self.age = age
+#         self.addr = addr
+#         print(f'学生{i}信息录入完成✅，信息为：【学生姓名：{self.name}年龄：{self.age}地址:{self.addr}】')
+# for i in range(1, 4):
+#     print(f'当前录入第{i}位学生信息')
+#     n = input('请输入学生姓名：')
+#     a = input('请输入学生年龄：')
+#     ad = input('请输入学生地址：')
+#     stu = Student(n, a, ad)
 
 
