b71eb6f46273c3062c8491494bf5f759cbac0595
Ran Shen <ranshen0519@icloud.com>
2022-09-24 08:32:22 +0800

    Changed

--- Python/经典习题/随机生成密码.py
+++ Python/经典习题/随机生成密码.py
@@ -10,4 +10,19 @@ import random
 random.seed(0x1010)
 s = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
 lst = []
-excludes = ''
+excludes = ''  # 用于首位排除
+
+while len(lst) < 10:  # 产生十个密码
+    passwords = ''
+    for i in range(10):  # 产生十位密码
+        passwords += s[random.randint(0, len(s) - 1)]
+    if passwords[0] in excludes:
+        continue
+    else:
+        lst.append(passwords)
+        excludes += passwords[0]
+
+# 存储
+with open('随机密码.txt', 'w') as fw:
+    fw.write('\n'.join(lst))
+
