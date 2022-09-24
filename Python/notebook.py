34aabd8f9211c74f0f7aadcc70765f7e7b192f72
Ran Shen <ranshen0519@icloud.com>
2022-09-24 18:32:07 +0800

    Changed

--- Python/notebook.py
+++ Python/notebook.py
@@ -390,18 +390,18 @@
 
 
 # 练习：设计一个带有私有成员的手机
-class Phone:
-    __is_5g_enable = False
+# class Phone:
+#     __is_5g_enable = False
 
-    def __check_5g(self):
-        if self.__is_5g_enable:
-            print('5g开启')
-        else:
-            print('5g关闭，使用4g网络')
+    # def __check_5g(self):
+    #     if self.__is_5g_enable:
+    #         print('5g开启')
+    #     else:
+    #         print('5g关闭，使用4g网络')
 
-    def call_by_5g(self):
-        self.__check_5g()
-        print('正在通话中')
+    # def call_by_5g(self):
+    #     self.__check_5g()
+    #     print('正在通话中')
 
-phone = Phone()
-phone.call_by_5g()
+# phone = Phone()
+# phone.call_by_5g()
