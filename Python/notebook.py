7d04440aa3a9417501d212edbcc457bb08f6730e
Ran Shen <ranshen0519@icloud.com>
2022-09-25 10:04:05 +0800

    Changed

--- Python/notebook.py
+++ Python/notebook.py
@@ -468,25 +468,28 @@
 
 
 # 复写(只可以在子类内部调用父类的同名成员）
-class Phone:
-    IMEI = None  # 序列号
-    producer = 'ITCAST'  # 厂商
+# class Phone:
+#     IMEI = None  # 序列号
+#     producer = 'ITCAST'  # 厂商
 
-    def call_by_5g(self):
-        print('使用5g网络进行通话')
+    # def call_by_5g(self):
+    #     print('使用5g网络进行通话')
 
-class MyPhone(Phone):
-    producer = 'ITHEIMA'  # 复写父类的成员属性
+# class MyPhone(Phone):
+#     producer = 'ITHEIMA'  # 复写父类的成员属性
 
-    def call_by_5g(self):
-        print('开启CPU单核模式，确保通话时省电')
+    # def call_by_5g(self):
+    #     print('开启CPU单核模式，确保通话时省电')
         # 调用父类同名成员(方式一）
         # print(f'父类的厂商是：{Phone.producer}')
         # Phone.call_by_5g(self)  # 注意要写self
         # 方式二
-        print(f'父类的厂商是：{super().producer}')
-        super().call_by_5g()
-        print('关闭CPU单核模式，确保性能 ')
-phone = MyPhone()
-phone.call_by_5g()
-print(phone.producer)
+        # print(f'父类的厂商是：{super().producer}')
+        # super().call_by_5g()
+        # print('关闭CPU单核模式，确保性能 ')
+# phone = MyPhone()
+# phone.call_by_5g()
+# print(phone.producer)
+
+
+# 调用方法传参的时候快捷键：command + p
