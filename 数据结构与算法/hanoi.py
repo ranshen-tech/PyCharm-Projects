49d4432b5bb131115ed7c6754b98a9d168bbff9d
Ran Shen <ranshen0519@icloud.com>
2022-09-30 13:11:14 +0800

    Changed

--- 数据结构与算法/hanoi.py
+++ 数据结构与算法/hanoi.py
@@ -51,5 +51,23 @@
 
 
 # 排序算法
+import random
+
+
 def bubble_sort(lst):
-for i in range(len(lst) - 1):  # 第i趟
+    for i in range(len(lst) - 1):  # 第i趟
+        exchange = False
+        for j in range(len(lst) - 1 - i):
+            if lst[j] > lst[j + 1]:
+                lst[j], lst[j + 1] = lst[j + 1], lst[j]
+                exchange = True
+        print(ls)
+        if not exchange:
+            return
+
+
+# ls = [random.randint(0, 100) for i in range(10)]
+ls = [1, 2, 3, 4, 5]
+print(ls, '\n')
+bubble_sort(ls)
+print(ls)
