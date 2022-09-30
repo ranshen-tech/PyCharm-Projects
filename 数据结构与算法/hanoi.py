a459ed15a5a8bc48e9878e21718a38fc7ededfb2
Ran Shen <ranshen0519@icloud.com>
2022-09-30 11:54:17 +0800

    Changed

--- 数据结构与算法/hanoi.py
+++ 数据结构与算法/hanoi.py
@@ -16,7 +16,7 @@
 #     """
 #     线性查找
 #     :param ls: list
-#     :param val: 要查找的值
+#     :param value: 要查找的值
 #     """
 #     for i, v in enumerate(ls):
 #         if v == value:
@@ -25,20 +25,31 @@
 #         return None
 
 
-def binary_search(ls, value):
-    left = 0
-    right = len(ls) - 1
-    while left <= right:  # 候选区有值
-        mid = (left + right) // 2
-        if ls[mid] == value:
-            return mid
-        elif value < ls[mid]:  # 待查找的值在mid左侧
-            right = mid - 1
-        else:  # mid < value 待查找的值在mid右侧
-            left = mid + 1
-    else:
-        return None
+# def binary_search(ls, value):
+#     """
+#     二分查找
+#     :param ls: list
+#     :param value: value
+#     :return: None/index
+#     """
+#     left = 0
+#     right = len(ls) - 1
+#     while left <= right:  # 候选区有值
+#         mid = (left + right) // 2
+#         if ls[mid] == value:
+#             return mid
+#         elif value < ls[mid]:  # 待查找的值在mid左侧
+#             right = mid - 1
+#         else:  # mid < value 待查找的值在mid右侧
+#             left = mid + 1
+#     else:
+#         return None
 
 
-lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-print(binary_search(lst, 5))
+# lst = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# print(binary_search(lst, 3))
+
+
+# 排序算法
+def bubble_sort(lst):
+for i in range(len(lst) - 1):  # 第i趟
