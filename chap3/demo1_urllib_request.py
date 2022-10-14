6655ef1665b7668c4e920ea66b6ae0efaa424737
Ran Shen <ranshen0519@icloud.com>
2022-10-14 17:39:34 +0800

    changed

--- chap3/demo1_urllib_request.py
+++ chap3/demo1_urllib_request.py
@@ -1,7 +1,8 @@
 # 教育机构 ：马士兵教育
 # 讲    师：杨淑娟
+import ssl
 import urllib.request
-
+ssl._create_default_https_context = ssl._create_unverified_context
 url = 'https://www.baidu.com/'
 # headers = {
 #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
--- chap3/demo2_urllib_request2.py
+++ chap3/demo2_urllib_request2.py
@@ -2,6 +2,8 @@
 # 讲    师：杨淑娟
 import urllib.parse
 import urllib.request
+# import ssl
+# ssl._create_default_https_context = ssl._create_unverified_context
 
 url = 'https://www.xslou.com/login.php'
 
