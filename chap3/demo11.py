375f42171b1e02b2ea97d67a48e92b468922aace
Ran Shen <ranshen0519@icloud.com>
2022-10-14 18:23:32 +0800

    changed

--- chap3/demo11.py
+++ chap3/demo11.py
@@ -1,9 +1,10 @@
-#教育机构 ：马士兵教育
-#讲    师：杨淑娟
+# 教育机构 ：马士兵教育
+# 讲    师：杨淑娟
 
-import  requests
-url='https://www.so.com/s'
-params={'q':'python'}
-resp=requests.get(url,params=params)
-resp.encoding='utf-8'
+import requests
+
+url = 'https://www.so.com/s'
+params = {'q': 'python'}
+resp = requests.get(url, params=params)
+resp.encoding = 'utf-8'
 print(resp.text)
--- chap3/demo12.py
+++ chap3/demo12.py
@@ -1,8 +1,8 @@
-#教育机构 ：马士兵教育
-#讲    师：杨淑娟
-import  requests
-url='https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=%E7%BE%8E%E5%A5%B3&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&hd=&latest=&copyright=&word=%E7%BE%8E%E5%A5%B3&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&cg=girl&pn=240&rn=30&gsm=f0&1592041096731='
-resp=requests.get(url)
-json_data=resp.json()
-print(json_data)
+# 教育机构 ：马士兵教育
+# 讲    师：杨淑娟
+import requests
 
+url = 'https://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=%E7%BE%8E%E5%A5%B3&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&hd=&latest=&copyright=&word=%E7%BE%8E%E5%A5%B3&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&force=&cg=girl&pn=240&rn=30&gsm=f0&1592041096731='
+resp = requests.get(url)
+json_data = resp.json()
+print(json_data)
