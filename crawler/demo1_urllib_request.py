37399547fdb9fc1f475e41db1722daeb9b7eaee3
Ran Shen <ranshen0519@icloud.com>
2022-10-15 16:28:50 +0800

    changed

--- crawler/demo1_urllib_request.py
+++ crawler/demo1_urllib_request.py
@@ -1,10 +1,10 @@
-# 教育机构 ：马士兵教育
-# 讲    师：杨淑娟
 import ssl
 import urllib.request
-ssl._create_default_https_context = ssl._create_unverified_context
+# ssl._create_default_https_context = ssl._create_unverified_context
+
+
 url = 'https://www.baidu.com/'
 # 发送请求
-resp = urllib.request.urlopen(url)
-html = resp.read().decode('gbk')  # decode将bytes类型转成str类型
+response = urllib.request.urlopen(url, context=ssl._create_unverified_context())
+html = response.read().decode('gbk')  # decode将bytes类型转成str类型
 print(html)
