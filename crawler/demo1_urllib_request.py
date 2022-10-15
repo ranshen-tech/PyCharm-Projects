27e4b67f30856cc18383538873247eb5aa56bf22
Ran Shen <ranshen0519@icloud.com>
2022-10-15 14:21:49 +0800

    changed

--- crawler/demo1_urllib_request.py
+++ crawler/demo1_urllib_request.py
@@ -4,9 +4,6 @@ import ssl
 import urllib.request
 ssl._create_default_https_context = ssl._create_unverified_context
 url = 'https://www.baidu.com/'
-# headers = {
-#     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36'
-# }
 # 发送请求
 resp = urllib.request.urlopen(url)
 html = resp.read().decode('gbk')  # decode将bytes类型转成str类型
