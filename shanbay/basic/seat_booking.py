23027d6c980b420fe17463bfac803aac2684d0ce
Ran Shen <ranshen0519@icloud.com>
2022-11-02 13:34:42 +0800

    changed

--- shanbay/basic/seat_booking.py
+++ shanbay/basic/seat_booking.py
@@ -4,8 +4,23 @@ import time
 
 
 class SeatBooking:
+    def get_row(self):
+        input_row = int(input("预订第几排的座位呢？请输入 1～6 之间的数字"))
+        valid_row = [i + 1 for i in range(6)]
+        while input_row not in valid_row:
+            input_row = int(input('没有按要求输入哦，请输入 1～6 之间的数字'))
+        row = input_row - 1
+        return row
 
-    # 展示所有座位的预订信息
+    def get_col(self):
+        input_column = int(input('预订这一排的第几座呢？请输入 1～8 之间的数字'))
+        valid_column = [i + 1 for i in range(8)]
+        while input_column not in valid_column:
+            input_column = int(input('没有按要求输入哦，请输入 1～8 之间的数字'))
+        column = input_column - 1
+        return column
+
+    # 展示座位预订信息
     def check_bookings(self, seats):
         print("正在为您查询该场次电影的预订状态...")
         time.sleep(0.7)
@@ -17,17 +32,14 @@ class SeatBooking:
         print("======================")
         time.sleep(0.7)
 
-    # 指定座位号预订座位
+    # 预订座位
     def book_seat(self, seats):
-        # 获取用户输入，转换为座位列表的行索引和列索引
-        row = int(input("预订第几排的座位呢？请输入 1～6 之间的数字")) - 1
-        column = int(input("预订这一排的第几座呢？请输入 1～8 之间的数字")) - 1
-
-        # 如果列表中对应值为 '○' 则预订成功，否则预订失败
+        row = self.get_row()
+        column = self.get_col()
         if seats[row][column] == '○':
             print("正在为您预订指定座位...")
             time.sleep(0.7)
             seats[row][column] = '●'  # 预订成功，该座位将被标记为“已预约”
-            print("预订成功！座位号：{}排{}座".format(str(row + 1), str(column + 1)))
+            print(f"预订成功！座位号：{row+1}排{column+1}座")
         else:
             print("这个座位已经被预订了哦")
