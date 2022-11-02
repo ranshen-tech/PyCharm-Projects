22cae5a237b247be6d0fd8b49a9bc8b130c24736
Ran Shen <ranshen0519@icloud.com>
2022-11-02 20:20:02 +0800

    changed

--- shanbay/basic/lesson30_version1.py
+++ shanbay/basic/lesson30_version1.py
@@ -1,11 +1,14 @@
 __author__ = 'ranshen0519@icloud.com'
-from infos import infos
+
+import time
+
 from film_selector import FilmSelector
 from seat_booking import SeatBooking
 
 
 class Controller:
     def __init__(self, information):
+        self.choice = None
         self.films = information  # 电影库所有电影
         self.welcome()
         self.choose_film()
@@ -16,19 +19,32 @@ class Controller:
     def choose_film(self):
         selector = FilmSelector()
         selector.display_options(self.films)
-        selector.get_choice(self.films)
+        self.choice = selector.get_choice(self.films)
 
     def choose_seat(self):
+        film = self.films[int(self.choice) - 1]
+        name = film['name']
+        symbol = film['symbol']
+        seat_list = film['seats']
         booking = SeatBooking()
-        booking.check_bookings()
-        booking.book_seat()
+        booking.check_bookings(seat_list)
+        booking.book_seat(seat_list)
 
-    def welcome(self):
-        pass
+    @staticmethod
+    def welcome():
+        print('+============================+')
+        print('+     欢迎来到时光电影院     +')
+        print('+============================+')
+        print('')
+        time.sleep(0.7)
 
+    @staticmethod
     def bye(self):
-        pass
-
+        print('')
+        time.sleep(0.2)
+        print('+============================+')
+        print('+  已经退出系统，下次见！👋  +')
+        print('+============================+')
 
 # seat_list = infos.infos[0]['seats']
 # booking = SeatBooking()
