2f8de60c1dee48d00b102b37cdd114162aee1b91
Ran Shen <ranshen0519@icloud.com>
2022-11-02 11:20:40 +0800

    changed

--- shanbay/basic/lesson29_assignment.py
+++ shanbay/basic/lesson29_assignment.py
@@ -1,17 +1,31 @@
 __author__ = 'ranshen0519@icloud.com'
-
 import turtle
-
-turtle.shape('turtle')
-turtle.color('red')
-turtle.hideturtle()
+from random import randint
 
 
-def draw_polygon(n):
-    for i in range(n):
-        turtle.forward(20)
-        turtle.right(360 / n)
+def draw_star():
+    turtle.color('blue')
+    turtle.hideturtle()
+    turtle.begin_fill()
+    for _ in range(5):
+        turtle.forward(10)
+        turtle.right(144)
+    turtle.end_fill()
 
 
-draw_polygon(20)
+# 将 ??? 改成你想画的星星个数
+for i in range(99):
+    turtle.speed(0)
+    turtle.penup()
+    x = randint(-150, 150)
+    y = randint(-100, 100)
+    turtle.goto(x, y)
+    turtle.pendown()
+    draw_star()
+
+turtle.penup()
+turtle.goto(0, -130)
+turtle.pendown()
+turtle.write('一闪一闪亮晶晶', font=('SimHei', 12, 'bold'))
 turtle.done()
+
