690bb5b43cc1b8687ee63a3fc3ead72b766c677e
Ran Shen <ranshen0519@icloud.com>
2022-11-12 14:02:27 +0800

    changed

--- Python3网络爬虫开发实战/第二章 - 基本库的使用/logging 模块用法.py
+++ Python3网络爬虫开发实战/第二章 - 基本库的使用/logging 模块用法.py
@@ -6,14 +6,23 @@ import logging
 
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s %(filename)s %(levelname)s %(message)s',
-                    datefmt='%a %d %b %Y %H:%M:%S',
-                    filename='my.log',
-                    filemode='w')
+                    # datefmt='%a %d %b %Y %H:%M:%S',
+                    datefmt='%Y %m %d %H:%M',
+                    # filename='my.log',
+                    # filemode='w'
+                    )
 
+# logging.basicConfig(
+                    # filename=path + 'log_' + today_date + '.txt',
+                    # level=logging.DEBUG, filemode='a',
+                    # format='%(asctime)s %(levelname)s >>>  %(message)s',
+                    # datefmt='%Y-%m-%d %H:%M')
+
+url = 'www.baidu.com'
+logging.info('scraping %s...', url)
 # logging.info('This is a info.')
 # logging.debug('This is a debug message.')
 # logging.warning('This is a warning.')
 # 输出到同目录下 my.log 文件中的内容：
 # Wed 05 Jun 2019 22:25:32 test.py INFO This is a info.
 # Wed 05 Jun 2019 22:25:32 test.py WARNING This is a warning.
-kk
